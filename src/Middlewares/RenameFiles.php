8950479bff5a85a5db82488b2a10b2d880a6ef3e
melodyxpot <melodyxpot@gmail.com>
2022-01-09 19:53:15 -0300

    Update in rename

--- src/Middlewares/RenameFiles.php
+++ src/Middlewares/RenameFiles.php
@@ -7,7 +7,7 @@ class RenameFiles
 
     public static function renameImage(string $name, string $image): string
     {
-        $fileName = md5($name) . $image;
+        $fileName = md5($name) .'.'. pathinfo($image)['extension'];
         return $fileName;
     }
 
